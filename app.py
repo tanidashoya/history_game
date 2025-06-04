d9b57d1b718cbc82cb11002e5230d733fec85b4a
tanidashoya <t120575st@gmail.com>
2025-06-04 22:14:09 +0900

    中級問題追加・home作成

--- app.py
+++ app.py
@@ -23,8 +23,12 @@ def before():
     if request.method == "GET" and "round" not in session:
         session.clear()
 
-# 最初に表示されるページ
 @app.route("/",methods=["GET","POST"])
+def home():
+    return render_template("home.html")
+
+# クイズ表示(もとは"/"だったが"indexに変更した")
+@app.route("/index",methods=["GET","POST"])
 def index():
 
     #sessionにroundがなくて、methodがGET(ページアクセス（見るだけ）)のときに実行
@@ -51,11 +55,11 @@ def index():
         return redirect(url_for("index"))     #新しくいれたコード
 
         
-    if session["round"] >= 15:
+    if session["round"] >= 5:
         return redirect("/finish")
     
-   
-    rd_quiz = choice(quiz_dict_list)  #同じブラウザ内（クッキー内）では同じ問題が出ないようにしたい
+    quiz_dict_list_biginner = quiz_dict_list[:500]  #初級問題
+    rd_quiz = choice(quiz_dict_list_biginner)  #同じブラウザ内（クッキー内）では同じ問題が出ないようにしたい
     quiz_inst = Quiz(rd_quiz["番号"], rd_quiz["問題"], rd_quiz["選択肢"], rd_quiz["正解"])   #quizインスタンスを作成 
      #sessionに直接インスタンスは渡せないのでそれぞれ属性ごとに渡す
     session["quiz"] = quiz_inst.quiz
@@ -121,21 +125,67 @@ def explanation():
                 )
             }
         ],
-            temperature=1.0,
-            presence_penalty=1.0,
-            frequency_penalty=1.0,
-            max_tokens=1800
+            # temperature=1.0,
+            # presence_penalty=1.0,
+            # frequency_penalty=1.0,
+            max_tokens=2000
             
             )
         res.append(response.choices[0].message.content)
         
     return render_template("explanation.html",incorrect_result = incorrect_result)
 
+#再スタート
 @app.route("/start",methods=["GET","POST"])
 def start():
     session.clear()
-    return redirect(url_for("index"))
+    return redirect(url_for("home"))
     
+    
+#中級問題
+@app.route("/intermediate",methods=["GET","POST"])
+def intermediate():
+
+    #sessionにroundがなくて、methodがGET(ページアクセス（見るだけ）)のときに実行
+    if "round" not in session and request.method == "GET":
+        session["round"] = 0        #一定回数POSTを受けたら抜けられるように回数を記録しておく用のsession
+        session["correct_count"] = 0   #正答数カウント用リスト
+        session["result"] = []  #結果保管用リスト
+        session["incorrect_result"] = [] #誤答した結果の辞書を格納するリスト
+
+    #methodがデータ送信（何かを渡す・登録する）のときに実行
+    if request.method == "POST":
+        user_input = request.form.get("user_input")  #ユーザーの選択した値を受け取る
+        quiz_dic = {"quiz": session["quiz"],"choices": session["choices"],"answer": session["answer"]}
+        result = session.get("result",[])
+        result.append([quiz_dic, user_input, session["round"]+1])  #result = [クイズの内容、ユーザーの選択肢、問題数]
+        session["result"] = result #sessuinに再代入
+        if user_input == quiz_dic["answer"]:
+            session["correct_count"] += 1
+        if user_input != quiz_dic["answer"]:
+            incorrect_result = session.get("incorrect_result",[])
+            incorrect_result.append([quiz_dic, user_input, session["round"]+1])
+            session["incorrect_result"] = incorrect_result
+        session["round"] += 1       #一回目のポストでsession["round"] = 1になる
+        return redirect(url_for("index"))     #新しくいれたコード
+
+        
+    if session["round"] >= 5:
+        return redirect("/finish")
+    
+    quiz_dict_list_biginner = quiz_dict_list[500:800]  #中級問題
+    rd_quiz = choice(quiz_dict_list_biginner)  #同じブラウザ内（クッキー内）では同じ問題が出ないようにしたい
+    quiz_inst = Quiz(rd_quiz["番号"], rd_quiz["問題"], rd_quiz["選択肢"], rd_quiz["正解"])   #quizインスタンスを作成 
+     #sessionに直接インスタンスは渡せないのでそれぞれ属性ごとに渡す
+    session["quiz"] = quiz_inst.quiz
+    session["choices"] = quiz_inst.choices
+    session["answer"] = quiz_inst.answer
+    
+    
+    round_count = session["round"] + 1        
+    #quizインズタンスをHTML側に渡して問題文を表示することはできないのでsessionの値を渡す
+    return render_template("intermediate.html", quiz = session["quiz"], choices = session["choices"], answer = session["answer"], round_count = round_count)
+
 
 
 if __name__ == "__main__":
@@ -146,11 +196,12 @@ if __name__ == "__main__":
 """gloval変数を使っていると複数ユーザーがアクセスする際に高確率でバグが発生する
 →sessionに保存することで解決"""""
 
+"""homeページを完成させる"""
 """問題数を増やす中級・上級問題も作成する(出題ページを分ける)"""
 """記述問題もつくる？"""
-<<<<<<< Updated upstream
-=======
+
 """問題をもう一度解くようなボタンを/finishにつくる"""
 """PC用語問題アプリを創る"""
->>>>>>> Stashed changes
+
+"""htmlのcss参照先をjinjaテンプレートで書き直す"""
 """chatgptのapiキーとsessionのキーをVPSの環境変数に設定できるようにする"""
